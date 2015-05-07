8977d1ef7a6c8e9875a0aaa9de4a6c13fccbbe58
Adam <adam.green@findly.com>
2015-05-07 16:44:46 +1200

    Initial project build

---
+++ src/HelloTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloTest.Startup))]
+namespace HelloTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
